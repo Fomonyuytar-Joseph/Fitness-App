f3407eb3b048975e5d4db68f9f8d3b1b0e6c29b9
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-07-15 16:06:34 +0100

    init

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,6 +2,32 @@ const  express = require('express')
 
 const router = express.Router()
 
-router.get('/',()=>{})
+
+//get all workpout
+router.get('/',(req, res)=>{
+    res.json({mssg:'get all workout'})
+})
+
+
+//get a single workout
+router.get('/:id',(req, res)=>{
+    res.json({mssg:'get a single work out workout'})
+})
+
+
+//POST a new workout
+router.post('/',(req,res)=>{
+    res.json({mssg:'POST a new work pout'})
+})
+
+//delete a workout
+router.delete('/:id',(req,res)=>{
+    res.json({mssg:'DELETE a  work pout'})
+})
+
+//update a work out
+router.patch('/:id',(req,res)=>{
+    res.json({mssg:'UPDATE a  workout'})
+})
 
 module.exports = router
