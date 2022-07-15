f522a725b1758ad0892a9cd72ee255a9ea4040d9
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-07-15 14:53:57 +0100

    init

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,12 @@ const express = require('express')
 //express app
 const app = express();
 
+app.get('/',(req, res)=>{
+
+    res.json({mssg:'welcome to the home page'})
+
+})
+
 //listen for request
 app.listen(4000, ()=>{
     console.log('listenx to server 4000')
