7b2e7461fb4bd6b24bb019ebffcf3b83f2e6be54
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-07-16 12:40:41 +0100

    init

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,61 +1,51 @@
-const Workout = require('../models/workoutModel')
-const mongoose = require('mongoose')
-
+const Workout = require("../models/workoutModel");
+const mongoose = require("mongoose");
 
 //get all workout
 
-const getWorkouts = async (req , res)=>{
-
-    const workouts = await Workout.find({}).sort({createdAt: -1});
-    res.status(200).json(workouts)
-}
-
-
+const getWorkouts = async (req, res) => {
+  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+  res.status(200).json(workouts);
+};
 
 //get a single workout
-const getWorkout = async (req ,res)=>{
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
+  
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such workout'})
+  }
 
-    const {id}= req.params
+  const workout = await Workout.findById(id);
 
-    const workout= await Workout.findById(id);
+  if (!workout) {
+    return res.status(404).json({ error: "work out not found" });
+  }
 
-    if(!workout){
-
-        return res.status(404).json({error:'work out not found'})
-    }
-
-    res.status(200).json(workout)
-}
+  res.status(200).json(workout);
+};
 
 //create a new workout
-const createWorkout = async (req, res)=>{
-    
-        const {title ,reps ,load}= req.body
-    
-    
-        //add doc to db
-        try{
-          const workouts =  await Workout.create({title,reps ,load})
-          res.status(200).json(workouts)
-    
-        }catch (error){
-          res.status(400).json({error:error.message})
-    
-        }
-      // res.json({mssg: 'POST a new workout'})
-   
+const createWorkout = async (req, res) => {
+  const { title, reps, load } = req.body;
 
-}
+  //add doc to db
+  try {
+    const workouts = await Workout.create({ title, reps, load });
+    res.status(200).json(workouts);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+  // res.json({mssg: 'POST a new workout'})
+};
 
 //delete a workout
 
 //update a workout
 
-
-
 module.exports = {
-     getWorkouts,
-    getWorkout,
+  getWorkouts,
+  getWorkout,
 
-    createWorkout
-}
+  createWorkout,
+};
