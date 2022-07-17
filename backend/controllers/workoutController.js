a59103b60f4fe35f6452de39d59afe771a7d1c32
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-07-17 14:53:55 +0100

    init

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,7 +1,7 @@
 const Workout = require("../models/workoutModel");
 const mongoose = require("mongoose");
 
-//get all workout
+//get all workouts
 
 const getWorkouts = async (req, res) => {
   const workouts = await Workout.find({}).sort({ createdAt: -1 });
